ff28fd55e1066ac6783a6a1b0655e5fa5e483c02
Pe-ter-M <peterwahu3491@gmail.com>
2025-10-30 21:22:51 +0300

    more

--- app/models/user.py
+++ app/models/user.py
@@ -8,6 +8,7 @@ class UserRole(enum.Enum):
     ADMIN = 'admin'
     STAFF = 'staff'
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False, index=True)
